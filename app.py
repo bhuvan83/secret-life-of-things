a95dde7bab5b6013f410c1e099f9495c34a19ac1
bhuvan83 <45890589+bhuvan83@users.noreply.github.com>
2026-04-21 22:43:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ if uploaded_file:
                 headers = {"xi-api-key": api_key, "Content-Type": "application/json"}
                 data = {
                     "text": rant,
-                    "model_id": "eleven_monolingual_v1",
+                    "model_id": "eleven_multilingual_v2",
                     "voice_settings": {"stability": 0.5, "similarity_boost": 0.5}
                 }
                 
